c4999d0e49add2b16a1108afafe89ed7f3859d9b
BatyLeo <leo.baty67@gmail.com>
2021-03-21 14:58:20 +0100

    code bugs fix

--- projet7/src/instance.jl
+++ projet7/src/instance.jl
@@ -8,12 +8,39 @@ struct Instance
     demand::Array{Int64, 2} # demand[j, t]
     precedence::Array{Array{Int64, 1}, 1} # precedence[j]
     rendements::Array{Int64, 4}  # rendements[l, a, i, j]
+    initial_l::Array{Int64, 1}
+end
+
+
+function create_instance()
+    T::Int64 = 10
+    P::Int64 = 40
+    a_max::Int64 = 2
+    l_max::Int64 = 2
+    C1::Int64 = 1
+    C2::Int64 = 1
+    demand::Array{Int64, 2} = [
+        1200 0 1200 0 1200 0 1200 0 1200 0;
+        0 400 0 400 0 400 0 400 0 400;
+    ]
+    precedence::Array{Array{Int64, 1}, 1} = [[2], [1]]
+    rendements::Array{Int64, 4} = zeros(l_max, a_max, C1 + C2 + 1, C1 + C2)
+    rendements[1, 1, 3, 1] = 72
+    rendements[2, 1, 3, 1] = 120
+    rendements[1, 1, 3, 2] = 54
+    rendements[2, 1, 3, 2] = 90
+    rendements[1, 2, 2, 1] = 54
+    rendements[2, 2, 2, 1] = 90
+    rendements[1, 2, 1, 2] = 39
+    rendements[2, 2, 1, 2] = 65
+
+    return Instance(T, P, a_max, l_max, C1, C2, demand, precedence, rendements, [l_max for p=1:P])
 end
 
 function graph_from_instance(instance::Instance)
     C = instance.C1 + instance.C2
 
-    sommets = Array{Tuple{Int64, 3}}([])
+    sommets = Array{Tuple{Int64, Int64, Int64}}([])
     for l=1:instance.l_max
         push!(sommets, (l, 0, 0))
         for a=1:instance.a_max
@@ -28,7 +55,7 @@ function graph_from_instance(instance::Instance)
     arcs = Array{Tuple{Int64, 2}, 1}([])
     delta_plus = Array{Array{Int64, 1}, 1}([[] for i=1:nb_sommets])
     delta_moins = Array{Array{Int64, 1}, 1}([[] for i=1:nb_sommets])
-    adj = Array{Int64, 2}(zeros(nb_sommets, nb_sommets))
+    adj = Array{Bool, 2}(zeros(nb_sommets, nb_sommets))
     counter = 1
     for (index1, sommet1) in enumerate(sommets)
         for (index2, sommet2) in enumerate(sommets)
@@ -57,36 +84,4 @@ function graph_from_instance(instance::Instance)
     return sommets, adj, delta_plus, delta_moins
 end
 
-function create_instance()
-    T::Int64 = 10
-    P::Int64 = 40
-    a_max::Int64 = 2
-    l_max::Int64 = 2
-    C1::Int64 = 1
-    C2::Int64 = 1
-    demand::Array{Int64, 2} = [
-        1200 0;
-        0 400;
-        1200 0;
-        0 400;
-        1200 0;
-        0 400;
-        1200 0;
-        0 400;
-        1200 0;
-        0 400;
-    ]
-    precedence::Array{Array{Int64, 1}, 1} = [[2], [1]]
-    rendements::Array{Int64, 4} = zeros(l_max, a_max, C1 + C2 + 1, C1 + C2)
-    rendements[1, 1, 3, 1] = 72
-    rendements[2, 1, 3, 1] = 120
-    rendements[1, 1, 3, 2] = 54
-    rendements[2, 1, 3, 2] = 90
-    rendements[1, 2, 2, 1] = 54
-    rendements[2, 2, 2, 1] = 90
-    rendements[1, 2, 1, 2] = 39
-    rendements[2, 2, 1, 2] = 65
-
-    return Instance(T, P, a_max, l_max, C1, C2, demand, precedence, rendements)
-end
 
--- projet7/src/model.jl
+++ projet7/src/model.jl
@@ -1,4 +1,4 @@
-function generate_model(instance::Instance, initial_states::Array{Int64, 1})
+function generate_model(instance::Instance)
     model = Model(CPLEX.Optimizer)
 
     C = instance.C1 + instance.C2
@@ -9,8 +9,8 @@ function generate_model(instance::Instance, initial_states::Array{Int64, 1})
     P = instance.P
 
     function is_legal(u, v, t)
-        l1, a1, j1 = sommets(u)
-        l2, a2, j2 = sommets(v)
+        l1, a1, j1 = sommets[u]
+        l2, a2, j2 = sommets[v]
         return adj[u, v] && ((j2 > instance.C1 && T%2 == 0) || (j2 <= instance.C1 && T%2 == 1))
     end
 
@@ -22,10 +22,10 @@ function generate_model(instance::Instance, initial_states::Array{Int64, 1})
 
     x = @variable(model, x[u=1:N, v=1:N, t=1:T, p=1:P; is_legal(u, v, t)], Bin)
 
-    @objective(model, Min, sum(x[initial_states[p], v, 1, p] for p=1:P for v in delta_plus[initial_states[p]] if is_legal(u, v, 1)))
+    @objective(model, Min, sum(x[instance.initial_l[p], v, 1, p] for p=1:P for v in delta_plus[instance.initial_l[p]] if is_legal(instance.initial_l[p], v, 1)))
 
     # TODO : check flow contrainte between T and 1
-    @constraint(model, [p=1:P, u:1:N, t=1:T-1], sum(x[v, u, t, p] for v in delta_moins[u] if is_legal(v, u, t)) ==
+    @constraint(model, [p=1:P, u=1:N, t=1:T-1], sum(x[v, u, t, p] for v in delta_moins[u] if is_legal(v, u, t)) ==
                                                sum(x[u, v, t+1, p] for v in delta_plus[u] if is_legal(u, v, t+1)))
 
     @constraint(model, [j=1:C, t=1:T], sum(weight(u, v) * x[u, v, t, p] for p=1:P for v=1:N for u in delta_moins[v] if sommets[v][3] == j && is_legal(u, v, t)) >= instance.demand[j, t])
